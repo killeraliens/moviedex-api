20721619bb2198abc26ab404ad3d55caf2c7d416
killeraliens <killeraliens@outlook.com>
2019-11-10 21:41:22 -0700

    get endpoints

--- app.js
+++ app.js
@@ -16,14 +16,40 @@ app.use(function validateAuthToken(req, res, next) {
   if(!bearerToken || apiToken !== bearerToken) {
     return res.status(401).json({error: 'Unauthorized Request'});
   }
-
   next()
 })
 app.get('/movies', handleGetMovies)
+app.get('/movie', handleGetMovie)
 
 
 function handleGetMovies(req, res) {
   res.json(movies)
+}
+
+function handleGetMovie(req, res) {
+  const { genre = '', country = '', avg_vote } = req.query;
+  let results = [...movies];
+
+  if (genre) {
+    results = results.filter(movie => {
+      return movie.genre.toLowerCase().includes(genre.toLowerCase())
+    })
+  }
+
+  if (country) {
+    results = results.filter(movie => {
+      return movie.country.toLowerCase().includes(country.toLowerCase())
+    })
+  }
+
+  if (avg_vote) {
+    let num = Number.parseFloat(avg_vote)
+    Number.isNaN(num)
+      ? res.status(400).json({error: "avg_vote Must Be A Number"})
+      : results = results.filter(movie => movie.avg_vote >= num)
+  }
+
+  res.json(results)
 }
 
 module.exports = app
